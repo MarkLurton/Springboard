6b653d7fd29cd5c89aee9e44b6d8c9c5da08770d
MarkLurton <marklurton@gmail.com>
2022-04-03 16:56:09 -0500

    Code Along for Multiple Events

---
+++ CodeAlong/Multiple_Events/app.js
@@ -0,0 +1,46 @@
+// const removeButtons = document.querySelectorAll('li button');
+// const form = document.querySelector('#add-friend');
+// const input = document.querySelector('#first-name');
+// const friendList = document.querySelector('#friend-list')
+
+// for (let btn of removeButtons) {
+//     btn.addEventListener('click', function(event) {
+//         event.target.parentNode.remove();
+//     });
+// };
+
+
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     const newFriend = document.createElement('li');
+//     const removeBtn = document.createElement('button');
+//     newFriend.innerText = `${input.value} `;
+//     removeBtn.innerText  = 'UnFriend';
+//     newFriend.appendChild(removeBtn);
+//     input.value = '';
+//     friendList.appendChild(newFriend);
+//     removeBtn.addEventListener('click', function(event) {
+//         event.target.parentNode.remove();
+//     });
+// });
+
+const form = document.querySelector('#add-friend');
+const input = document.querySelector('#first-name');
+const friendList = document.querySelector('#friend-list')
+
+friendList.addEventListener('click', function(event) {
+    if ( event.target.tagName === "BUTTON" ) {
+        event.target.parentNode.remove();
+    }
+})
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const newFriend = document.createElement('li');
+    const removeBtn = document.createElement('button');
+    newFriend.innerText = `${input.value} `;
+    removeBtn.innerText  = 'UnFriend';
+    newFriend.appendChild(removeBtn);
+    input.value = '';
+    friendList.appendChild(newFriend);
+});

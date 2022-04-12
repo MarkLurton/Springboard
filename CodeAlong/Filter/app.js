96578dfba026bc53335b6431da4e6b39932a805e
MarkLurton <marklurton@gmail.com>
2022-04-12 10:40:11 -0500

    Code Along for Filter

---
+++ CodeAlong/Filter/app.js
@@ -0,0 +1,36 @@
+const words = [
+    'immunoelectrophoretically',
+    'rotovator',
+    'tsktsk',
+    'psychophysicotherapeutics',
+    'squirrelled',
+    'crypt',
+    'uncopyrightable',
+    'cysts',
+    'pseudopseudohypoparathyroidism',
+    'unimaginatively'
+];
+
+const longWords = words.filter(function(word) {
+    return (word.length >=20)
+});
+
+const noVowels = words.filter(function(word) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    for (let vowel of vowels) {
+        if (word.includes(vowel)) {
+            return false;
+        }
+    }
+    return true;
+});
+
+const containsVowels = words.filter(function(word) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    for (let vowel of vowels) {
+        if (word.includes(vowel)) {
+            return true;
+        }
+    }
+    return false;
+});

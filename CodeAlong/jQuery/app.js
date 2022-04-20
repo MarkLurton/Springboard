9f2784914715afe86344e5e8bef69006eee42635
MarkLurton <marklurton@gmail.com>
2022-04-20 17:44:52 -0500

    Code Along

---
+++ CodeAlong/jQuery/app.js
@@ -0,0 +1,35 @@
+// $("img").click(function () {
+//   alert("HELLO!!");
+// });
+
+// $("img").on("mouseleave", function () {
+//   console.log("LEAVING AN IMAGE!");
+// });
+
+$("img").on("mouseenter", function () {
+  $(this).css("border", "10px solid purple");
+});
+
+$("img").on("click", function () {
+  // $(this).fadeOut(3000, function () {
+  //   $(this).remove()
+  // })
+  $(this).animate(
+    {
+      opacity: 0,
+      width: "50px", //DO NOT ANIMATE WIDTH (POOR PERFORMANCE)
+    },
+    3000,
+    function () {
+      $(this).remove();
+    }
+  );
+});
+
+$("#add-input").on("click", function () {
+  $("form").append('<input type="text"/>');
+});
+
+$("form").on("focus", "input", function () {
+  $(this).val("BAMBOOZLED");
+});

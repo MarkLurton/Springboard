0088427ada02c384e8b24ce03462e4b6fad772be
MarkLurton <marklurton@gmail.com>
2022-07-31 20:08:19 -0500

    Jest Code Along

--- Section_2-Back_End/CodeAlong/node/firstNodeFile.js
+++ Section_2-Back_End/CodeAlong/node/firstNodeFile.js
@@ -1,7 +1,34 @@
-const annoyingGreet = () => {
-  for (let i = 0; i < 10; i++) {
-    console.log("HELLO FROM NODE!");
-  }
-};
+const fs = require("fs");
+// const annoyingGreet = () => {
+//   for (let i = 0; i < 10; i++) {
+//     console.log("HELLO FROM NODE!");
+//   }
+// };
 
-annoyingGreet();
+// annoyingGreet();
+
+// fs.readFile("test.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.log("ERROR: ", err);
+//     process.kill(1);
+//   }
+//   console.log("DATA... ", data);
+// });
+
+const line = "And Eternity in an hour";
+
+// fs.writeFile("test.txt", line, { encoding: "utf8", flag: "a" }, (err) => {
+//   if (err) {
+//     console.log("Error: ", err);
+//     process.kill(1);
+//   }
+//   console.log("Successfully wrote to test.txt!");
+// });
+
+fs.appendFile("test.txt", "\nAPPEND ME!!!", "utf8", (err) => {
+  if (err) {
+    console.log("ERROR!!!", err);
+    process.kill(1);
+  }
+  console.log("Appended!!");
+});
